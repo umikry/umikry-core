ced44a004bcf9857dd9fc3599d20227847deae13
Fabian Bormann <fabian_mb@gmx.de>
2018-11-08 17:44:45 +0100

    adjust detector to allow more methods

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # umikry-core
+
+umikry-core ships the detector, generator, data and feature_extractor methods to
+detect, generate and replace faces by different algorithms
+
+## Setup
+
+git clone https://github.com/umikry/umikry-core.git
+cd umikry-core
+pip(3) install -r requirements.txt
+
+## Usage
+
+```python
+from detection import UmikryFaceDetector
+
+umikryFaceDetector = UmikryFaceDetector(method='haar')
+image = cv2.imread('/path/to/my/image.jpg')
+faces = umikryFaceDetector.detect(image)
+
+# or even with method cnn
+
+config = configparser.ConfigParser()
+config.read('umikry.ini')
+if 'DATA' in config:
+	data_dir = config['DATA']['Location']
+else:
+	data_dir = input('Where is your train data located:')
+	config['DATA'] = {'Location': data_dir}
+	with open('umikry.ini', 'w') as configfile:
+		config.write(configfile)
+
+umikryFaceDetector = UmikryFaceDetector(method='cnn')
+
+generateTrainTestSet(data_dir, datasets=['OpenImages'])
+
+train_sequence = ImageSequence(data_dir, batch_size=16, patch_size=(384, 384), is_training=True)
+test_sequence = ImageSequence(data_dir, batch_size=16, patch_size=(384, 384))
+
+checkpoint_dir = 'models'
+model_checkpoint = ModelCheckpoint(os.path.join(checkpoint_dir, 'community_facedetector_weights.{epoch:02d}-{val_loss:.2f}.h5'), 
+                                 save_weights_only=True, save_best_only=True, monitor='val_acc')
+early_stopping = EarlyStopping(patience=5)
+
+umikryFaceDetector.train(train_sequence, epochs=20,
+                       test_generator=test_sequence,
+                       callbacks=[model_checkpoint, early_stopping])
+umikryFaceDetector.model.save_weights(os.path.join(checkpoint_dir, 'community_facedetector_weights.h5'))
+image = cv2.imread('/path/to/my/image.jpg')
+face_segmentation = umikryFaceDetector.detect(image)
+```
--- detection.py
+++ detection.py
@@ -1,7 +1,6 @@
 from keras.models import Model
 from keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D, Add, BatchNormalization, LeakyReLU, Conv2DTranspose
 from keras.callbacks import ModelCheckpoint, EarlyStopping
-from keras import backend as K
 from keras.utils import Sequence
 import numpy as np
 import configparser
@@ -9,9 +8,8 @@ import os
 import random
 import sys
 import math
-import time
 import cv2
-import matplotlib.pyplot as plt
+import wget
 
 
 def generateTrainTestSet(data_location, datasets, test_size=0.1, override=True):
@@ -49,14 +47,15 @@ def pad_to(image, n, axis=0):
     border_top = int(diff / 2)
     border_bottom = diff - border_top
     return (cv2.copyMakeBorder(image, border_top, border_bottom, 0, 0,
-                             cv2.BORDER_CONSTANT, value=0),
-          (border_top, border_bottom, 0, 0))
+            cv2.BORDER_CONSTANT, value=0),
+            (border_top, border_bottom, 0, 0))
   if axis == 1:
     border_left = int(diff / 2)
     border_right = diff - border_left
     return (cv2.copyMakeBorder(image, 0, 0, border_left, border_right,
-                             cv2.BORDER_CONSTANT, value=0),
-          (0, 0, border_left, border_right))
+            cv2.BORDER_CONSTANT, value=0),
+            (0, 0, border_left, border_right))
+
 
 def pad_to_next_multiply_of_n(image, n):
   diff_y = next_multiply_of_n(image.shape[0], n) - image.shape[0]
@@ -67,7 +66,7 @@ def pad_to_next_multiply_of_n(image, n):
   border_left = int(diff_x / 2)
   border_right = diff_x - border_left
 
-  padded_image = cv2.copyMakeBorder(image, border_top, border_bottom, border_left, border_right, 
+  padded_image = cv2.copyMakeBorder(image, border_top, border_bottom, border_left, border_right,
                                     cv2.BORDER_CONSTANT, value=0)
   return padded_image, (border_top, border_bottom, border_left, border_right)
 
@@ -91,6 +90,7 @@ def crop_random(image, truth, size=(512, 512)):
 
   return image, truth.reshape(image.shape[0], image.shape[1], 1)
 
+
 class ImageSequence(Sequence):
   def __init__(self, data_location, batch_size=32, is_training=False, patch_size=(512, 512), plain_images=False):
     if is_training:
@@ -110,9 +110,9 @@ class ImageSequence(Sequence):
     if self.plain_images:
       sample = self.dataset[i]
       image_file = sample.split(',')[0]
-      truth_file = sample.split(',')[1][:-1]        
+      truth_file = sample.split(',')[1][:-1]
       image = np.float32(cv2.imread(os.path.join(self.data_location, image_file)) / 255.0)
-      truth = cv2.imread(os.path.join(self.data_location, truth_file), cv2.IMREAD_GRAYSCALE) / 255.            
+      truth = cv2.imread(os.path.join(self.data_location, truth_file), cv2.IMREAD_GRAYSCALE) / 255.
       data, _ = pad_to_next_multiply_of_n(image, 8)
       label, _ = pad_to_next_multiply_of_n(truth, 8)
       return np.expand_dims(data, axis=0), np.expand_dims(label, axis=0)
@@ -131,43 +131,36 @@ class ImageSequence(Sequence):
 
 
 class UmikryFaceDetector():
-  def __init__(self, pretrained_weights=None, encoder_weights=None):
-    self.build()
+  def __init__(self, method='haar', pretrained_weights=None, encoder_weights=None):
+    self.method = method
 
-    if pretrained_weights is not None:
-      self.model.load_weights(pretrained_weights)
-    elif encoder_weights is not None:
-      self.model.load_weights(encoder_weights, by_name=True)
-    
-  def haar_face_detection(self, img, haar_classifier, scale_factor=1.1, min_neighbors=3, debug=False):
-    # img = cv2.imread('./data/OpenImages/images/{0}.jpg'.format(img), cv2.IMREAD_GRAYSCALE)
-    faces = haar_classifier.detectMultiScale(img)
-    faces_dict = {}
-    if debug:
-        plt.figure()
-        plt.imshow(img, cmap="gray")
-    if len(faces) > 0:
-        for i, (x, y, w, h) in enumerate(faces):
-            faces_dict[i] = [x, y, x+w, y+h]
-            if debug:
-                cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
-                plt.figure()
-                plt.imshow(img, cmap="gray")
-        return faces_dict
+    if self.method == 'cnn':
+      self.__build()
+      if pretrained_weights is not None:
+        self.model.load_weights(pretrained_weights)
+      elif encoder_weights is not None:
+        self.model.load_weights(encoder_weights, by_name=True)
+    elif self.method == 'haar':
+      haar_classifier_path = os.path.join('models', 'haarcascade_frontalface_default.xml')
+      if not os.path.isfile(haar_classifier_path):
+        haar_classifier_url = 'https://github.com/opencv/opencv/raw/master/data/haarcascades/haarcascade_frontalface_default.xml'
+        wget.download(haar_classifier_url, haar_classifier_path)
+
+      self.haar_classifier = cv2.CascadeClassifier(haar_classifier_path)
     else:
-        return None
+      raise ValueError('{} is an invalid method use \'cnn\' or \'haar\' instead'.format(self.method))
 
-  def build(self):
+  def __build(self):
     image = Input(shape=(None, None, 3))
     conv1 = Conv2D(16, 3, padding='same', name='d_conv_1')(image)
     conv1 = LeakyReLU(alpha=0.1, name='d_relu_1')(conv1)
     pool1 = MaxPooling2D(name='d_pool1')(conv1)
-    
+
     conv2 = Conv2D(32, 3, padding='same', use_bias=False, name='d_conv_2')(pool1)
     conv2 = BatchNormalization(momentum=0.8, name='d_bn_1')(conv2)
     conv2 = LeakyReLU(alpha=0.1, name='d_relu_2')(conv2)
     pool2 = MaxPooling2D(name='d_pool2')(conv2)
-    
+
     conv3 = Conv2D(64, 3, padding='same', use_bias=False, name='d_conv_3')(pool2)
     conv3 = BatchNormalization(momentum=0.8, name='d_bn_2')(conv3)
     conv3 = LeakyReLU(alpha=0.1, name='d_relu_3')(conv3)
@@ -177,70 +170,63 @@ class UmikryFaceDetector():
     conv4 = BatchNormalization(momentum=0.8, name='d_bn_3')(conv4)
     conv4 = LeakyReLU(alpha=0.1, name='d_relu_4')(conv4)
     pool4 = MaxPooling2D(name='d_pool4')(conv4)
-    
+
     conv5 = Conv2D(256, 3, padding='same', use_bias=False, name='d_conv_5')(pool4)
     conv5 = BatchNormalization(momentum=0.8, name='d_bn_4')(conv5)
     conv5 = LeakyReLU(alpha=0.1, name='d_relu_5')(conv5)
-    
+
     upscale1 = UpSampling2D(interpolation='bilinear')(conv5)
     conv6 = Conv2DTranspose(128, 3, padding='same', use_bias=False)(upscale1)
     conv6 = BatchNormalization()(conv6)
     conv6 = LeakyReLU(alpha=0.1)(conv6)
     fuse1 = Add()([conv6, conv4])
-    
+
     upscale2 = UpSampling2D(size=4, interpolation='bilinear')(fuse1)
     conv7 = Conv2DTranspose(32, 3, padding='same', use_bias=False)(upscale2)
     conv7 = BatchNormalization()(conv7)
     conv7 = LeakyReLU(alpha=0.1)(conv7)
-    
+
     upscale3 = UpSampling2D(interpolation='bilinear')(conv7)
     conv8 = Conv2DTranspose(16, 5, padding='same', use_bias=False)(upscale3)
     conv8 = BatchNormalization()(conv8)
     conv8 = LeakyReLU(alpha=0.1)(conv8)
     fuse2 = Add()([conv8, conv1])
-    
+
     final_score = Conv2D(1, 1, activation='sigmoid', padding='same')(fuse2)
 
     self.model = Model(image, final_score)
     self.model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['mse', 'accuracy'])
 
-  def predict(self, image):
-    image, border = pad_to_next_multiply_of_n(image[0], 8)
-    image = np.float32(image / 255.0)
-    prediction = self.model.predict(np.array([image]))[0]
-    prediction = prediction[border[0]:(prediction.shape[0] - border[1]),
-                            border[2]:(prediction.shape[1] - border[3]), :]
-    return prediction.reshape(prediction.shape[0], prediction.shape[1])
+  def __haar_detection(self, image, scale_factor=1.1, min_neighbors=3):
+    if len(image.shape) > 2:
+      image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-  def train(self, train_generator, epochs=10, steps_per_epoch=None, test_generator=None, validation_steps=None, callbacks=None):
-    self.model.fit_generator(train_generator, epochs=epochs, steps_per_epoch=steps_per_epoch,
-                             validation_data=test_generator, validation_steps=validation_steps,
-                             use_multiprocessing=True, workers=2, callbacks=callbacks)
+    faces = self.haar_classifier.detectMultiScale(image)
 
+    if len(faces) > 0:
+        faces_dict = {}
+        for i, (x, y, width, height) in enumerate(faces):
+          faces_dict[i] = (x, y, x + width, y + height)
+        return faces_dict
+    else:
+        return None
 
-if __name__ == '__main__':
-  config = configparser.ConfigParser()
-  config.read('umikry.ini')
-  if 'DATA' in config:
-    data_dir = config['DATA']['Location']
-  else:
-    data_dir = input('Where is your train data located:')
-    config['DATA'] = {'Location': data_dir}
+  def detect(self, image, scale_factor=1.1, min_neighbors=3):
+    if self.method == 'haar':
+      return self.__haar_detection(image)
+    elif self.method == 'cnn':
+      image, border = pad_to_next_multiply_of_n(image[0], 8)
+      image = np.float32(image / 255.0)
+      prediction = self.model.predict(np.array([image]))[0]
+      prediction = prediction[border[0]:(prediction.shape[0] - border[1]),
+                              border[2]:(prediction.shape[1] - border[3]), :]
+      return prediction.reshape(prediction.shape[0], prediction.shape[1])
 
-    with open('umikry.ini', 'w') as configfile:
-      config.write(configfile)
-
-  umikryFaceDetector = UmikryFaceDetector()
-  generateTrainTestSet(data_dir, datasets=['OpenImages'])
-
-  train_sequence = ImageSequence(data_dir, batch_size=16, patch_size=(384, 384), is_training=True)
-  test_sequence = ImageSequence(data_dir, batch_size=16, patch_size=(384, 384))
-
-  checkpoint_dir = 'models'
-  model_checkpoint = ModelCheckpoint(os.path.join(checkpoint_dir, 'community_facedetector_weights.{epoch:02d}-{val_loss:.2f}.h5'), save_weights_only=True, save_best_only=True, monitor='val_acc')
-  early_stopping = EarlyStopping(patience=5)
-
-  umikryFaceDetector.train(train_sequence, epochs=20,
-                           test_generator=test_sequence,
-                           callbacks=[model_checkpoint, early_stopping])
-  umikryFaceDetector.model.save_weights(os.path.join(checkpoint_dir, 'community_facedetector_weights.h5'))
+  def train(self, train_generator, epochs=10, steps_per_epoch=None, test_generator=None,
+            validation_steps=None, callbacks=None):
+    if self.method == 'haar':
+      raise NotImplementedError
+    elif self.method == 'cnn':
+      self.model.fit_generator(train_generator, epochs=epochs, steps_per_epoch=steps_per_epoch,
+                               validation_data=test_generator, validation_steps=validation_steps,
+                               use_multiprocessing=True, workers=2, callbacks=callbacks)
